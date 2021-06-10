d8cf71ffd01d9a6bc15ac397f4ca4829b617fdb0
MicoPham <michaelxpham@gmail.com>
2021-06-09 17:28:49 -0700

    Add ability to clear and make new grid accordingly to answer

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,41 @@
 const container = document.querySelector('.container');
-const body = document.querySelector('body');
 
-const div = document.createElement('div');
-window.onload = function () {
-    for (i = 0; i < 256; i++) {
-      const div = document.createElement('div');
-      div.textContent = '';
-      container.appendChild(div);
+document.body.style.setProperty('--answer', answer);
+
+var answer = 16;
+grids(answer);
+
+function grids (number) {
+    for (i = 0; i < number ** 2; i++) {       
+        const div = document.createElement('div');
+        document.body.style.setProperty('--answer', answer);
+        container.appendChild(div);       
     }
-  }
+}
+
+function clear () {
+    container.innerHTML = "";
+}
+
+function askNumber () {
+    var question = Number(prompt("Enter a number between 4 to 100 for grid size", ""));
+    if (question >= 4 && question <= 100) {
+        answer = question;
+        return answer;
+    } else {
+        alert('try again');
+    }
+}
 
 container.addEventListener('mouseover', function (e) {
     e.target.style.background = 'black';
 })
 
-function clearcontent(elementID) {
-    document.querySelector(elementID).innerHTML = "";
-}
-
+const body = document.querySelector('body');
+const btn = document.createElement('button');
+btn.textContent = `New Grid`;
+body.prepend(btn);
+btn.addEventListener('click', () => {
+    clear();
+    grids(askNumber());
+})
